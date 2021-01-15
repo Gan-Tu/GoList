c02f248f0d84927592f44812e8873963f1c0a780
Gan Tu <github@tugan.dev>
2021-01-14 19:55:56 -0800

    fix test

--- client/src/components/input/UrlTextInput.test.js
+++ client/src/components/input/UrlTextInput.test.js
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import UrlTextInput from "./UrlTextInput";
 
 test("renders a input link", () => {
-  render(<UrlTextInput n={1} url="hello" />);
+  render(<UrlTextInput n={0} url="hello" />);
   const inputElement = screen.getByLabelText(/URL #1/i);
   expect(inputElement).toBeInTheDocument();
 });
