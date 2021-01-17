2c6f6f38cebbc6ff583ba5e55a2f42107921a173
Gan Tu <github@tugan.dev>
2021-01-16 17:45:08 -0800

    add link to home screen

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,23 @@ import logo from "./logo.svg";
 import "./App.css";
 import Listing from "./components/Listing";
 import UrlForm from "./components/input/UrlForm";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Container from 'react-bootstrap/Container';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Container from "react-bootstrap/Container";
 
 class App extends Component {
   render() {
     return (
       <Container fluid className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>GoList</h1>
-        </header>
         <Router>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>
+              <Link to="/" style={{ color: "#FFF" }}>
+                GoList
+              </Link>
+            </h1>
+          </header>
           <Route exact path="/" component={UrlForm} />
           <Route path="/:id" component={Listing} />
         </Router>
