9f1cf0741f5313dbb66a5cc14003d7e88be115e3
Gan Tu <github@tugan.dev>
2021-01-31 12:24:33 -0800

    add example alert card

--- v2/client/src/components/home/index.jsx
+++ v2/client/src/components/home/index.jsx
@@ -7,7 +7,7 @@ const Sample = (props) => {
     <Fragment>
       <Breadcrumb title="Home" />
       <Container fluid={true}>
-        <Row>
+        <Row className="appointment-sec">
           <Col sm="12">
             <Card>
               <CardHeader>
@@ -25,6 +25,21 @@ const Sample = (props) => {
               </CardBody>
             </Card>
           </Col>
+          <Col xl="6" className="alert-sec">
+                <Card className="bg-img">
+                  <CardHeader>
+                    <div className="header-top">
+                      <h5 className="m-0">{"Alert"}  </h5>
+                      <div className="dot-right-icon"><i className="fa fa-ellipsis-h"></i></div>
+                    </div>
+                  </CardHeader>
+                  <CardBody>
+                    <div className="body-bottom">
+                      <h6>  {"10% off For drama lights Couslations..."}</h6><span className="font-roboto">{"Lorem Ipsum is simply dummy...It is a long established fact that a reader will be distracted by "} </span>
+                    </div>
+                  </CardBody>
+                </Card>
+              </Col>
         </Row>
       </Container>
     </Fragment>
