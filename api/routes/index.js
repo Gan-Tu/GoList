e4fa726087ecb0e407a009c60023b89944252d81
Gan Tu <github@tugan.dev>
2021-02-07 19:41:01 -0800

    add basic GET API routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const { Datastore } = require("@google-cloud/datastore");
-const datastore = new Datastore();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Golist API' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Golist API" });
 });
 
 module.exports = router;
