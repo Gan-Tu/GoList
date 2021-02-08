c2630045820ba1eaccf99d58634d11321e80ff6f
Gan Tu <github@tugan.dev>
2021-02-07 18:18:44 -0800

    add google cloud datastore

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const { Datastore } = require("@google-cloud/datastore");
+const datastore = new Datastore();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
