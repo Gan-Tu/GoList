9276fc1d260e769a325a2a25277869afc3bd82eb
Gan Tu <github@tugan.dev>
2021-02-08 22:31:02 -0800

    uncomment seed.js code, with error handling of already exists for insert

--- api/seed.js
+++ api/seed.js
@@ -1,36 +1,39 @@
 const { Datastore } = require("@google-cloud/datastore");
 const datastore = new Datastore();
 
-// const SEED_DATA = [
-//   {
-//     key: datastore.key(["GoLists"]),
-//     data: {
-//       name: "tugan",
-//       title: "Gan is the best",
-//       date: new Date("29 March 1997"),
-//       owner: "Admin",
-//       hits: 100,
-//     },
-//   },
-//   {
-//     key: datastore.key(["GoLists"]),
-//     data: {
-//       name: "demo",
-//       title: "Lorem ipsum dolor sit amet, consectetur.",
-//       date: new Date("9 April 2019"),
-//       owner: "Admin",
-//       hits: 100,
-//     },
-//   },
-// ];
+const SEED_DATA = [
+  {
+    key: datastore.key(["GoLists", "test"]),
+    data: {
+      name: "tugan",
+      title: "Gan is the best",
+      date: new Date("29 March 1997"),
+      owner: "Admin",
+      hits: 100,
+    },
+  },
+  {
+    key: datastore.key(["GoLists"]),
+    data: {
+      name: "demo",
+      title: "Lorem ipsum dolor sit amet, consectetur.",
+      date: new Date("9 April 2019"),
+      owner: "Admin",
+      hits: 100,
+    },
+  },
+];
 
-// datastore.save(SEED_DATA, function (err, apiResponse) {
-//   console.log(SEED_DATA);
-//   if (err) {
-//     return next(err);
-//   }
-//   console.log(apiResponse);
-// });
+datastore.insert(SEED_DATA, function (err, apiResponse) {
+  console.log(SEED_DATA); 
+  if (err.code === /* ALREADY_EXISTS */ 6) {
+    console.log("already exists");
+  } else if (err) {
+    console.log(`error: ${err.message}`);
+  } else {
+    console.log(apiResponse);
+  }
+});
 
 // async function getData() {
 //   let query = datastore
