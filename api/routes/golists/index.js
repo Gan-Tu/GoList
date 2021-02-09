34735331a9bbeceffb42fa1e7349ac1a7e4fe1c1
Gan Tu <github@tugan.dev>
2021-02-08 23:02:56 -0800

    move name param validation check to a separate middleware

--- api/routes/golists/index.js
+++ api/routes/golists/index.js
@@ -1,33 +1,41 @@
 var express = require("express");
 var router = express.Router();
+
 const { Datastore } = require("@google-cloud/datastore");
-const { all } = require(".");
 const datastore = new Datastore();
 
-/* GET home page. */
-router
-  .route("/:name")
-  .get(function (req, res, next) {
-    if (!req.params.name && req.params.name.length <= 0) {
-      return res.status(400).json({ err: "name cannot be empty" });
-    }
-    let query = datastore
-      .createQuery("GoLists")
-      .filter("name", "=", req.params.name);
-    datastore.runQuery(query, (err, entities) => {
-      if (err) {
-        next(
-          `Encountered error when fetching Key(GoLists, name="${req.params.name}"): ${err}`
-        );
-      } else if (!entities.length) {
-        res
-          .status(404)
-          .json({ err: `No GoLists with name found: ${req.params.name}` });
-      } else {
-        res.json({ data: entities });
-      }
+const VALID_NAME_PATTERN = /^[a-zA-Z0-9]+[a-zA-Z0-9-]*$/;
+
+router.param("name", function (req, res, next, name) {
+  if (!name || name.length <= 0) {
+    return res.status(400).json({ err: "GoList name cannot be empty" });
+  } else if (!name.match(VALID_NAME_PATTERN)) {
+    return res.status(400).json({
+      err: "GoList name can only be alphanumeric, with non-leading dashes",
     });
+  } else {
+    next();
+  }
+});
+
+router.route("/:name").get(function (req, res, next) {
+  let query = datastore
+    .createQuery("GoLists")
+    .filter("name", "=", req.params.name);
+  datastore.runQuery(query, (err, entities) => {
+    if (err) {
+      next(
+        `Encountered error when fetching Key(GoLists, name="${req.params.name}"): ${err}`
+      );
+    } else if (!entities.length) {
+      res
+        .status(404)
+        .json({ err: `No GoLists with name found: ${req.params.name}` });
+    } else {
+      res.json({ data: entities });
+    }
   });
+});
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
