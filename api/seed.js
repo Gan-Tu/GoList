7f609a3328b19ad0d15f6aa8a03d6b7df25dda13
Gan Tu <github@tugan.dev>
2021-02-16 21:00:34 -0800

    modify seed script to show more cases

--- api/seed.js
+++ api/seed.js
@@ -22,7 +22,52 @@ const SEED_DATA = [
       hits: 100,
     },
   },
+  {
+    key: datastore.key(["GoLists", "demo", "nested", "01"]),
+    data: {
+      name: "demo-nested-01",
+      title: "Lorem ipsum dolor sit amet, consectetur.",
+      last_modified_date: new Date("9 April 2019"),
+      owner: "Admin",
+      hits: 100,
+    },
+  },
+  {
+    key: datastore.key(["GoLists", "demo", "nested", "02"]),
+    data: {
+      name: "demo-nested-02",
+      title: "Lorem ipsum dolor sit amet, consectetur.",
+      last_modified_date: new Date("9 April 2019"),
+      owner: "Admin",
+      hits: 100,
+    },
+  },
+  {
+    key: datastore.key(["nested", "03"]),
+    data: {
+      name: "nested-03",
+      title: "Lorem ipsum dolor sit amet, consectetur.",
+      last_modified_date: new Date("9 April 2019"),
+      owner: "Admin",
+      hits: 100,
+    },
+  },
 ];
+
+async function getData() {
+  let query = datastore
+    .createQuery("nested")
+    .hasAncestor(datastore.key(["GoLists", "demo"]));
+  // .filter("date", "<", new Date("29 March 2000"));
+  try {
+    const [tasks] = await datastore.runQuery(query);
+    console.log("Tasks:");
+    tasks.forEach((task) => console.log(task));
+  } catch (err) {
+    console.error(err);
+  }
+}
+
 
 datastore.save(SEED_DATA, function (err, apiResponse) {
   if (err && err.code === /* ALREADY_EXISTS */ 6) {
@@ -33,15 +78,5 @@ datastore.save(SEED_DATA, function (err, apiResponse) {
     console.log(SEED_DATA);
     console.log(apiResponse);
   }
+  getData();
 });
-
-// async function getData() {
-//   let query = datastore
-//     .createQuery("GoLists")
-//     .filter("date", "<", new Date("29 March 2000"));
-//   const [tasks] = await datastore.runQuery(query);
-//   console.log("Tasks:");
-//   tasks.forEach((task) => console.log(task));
-// }
-
-// getData();
