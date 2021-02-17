dd144bd3b469f9230cf6a33ea38ef2e835c6da43
Gan Tu <github@tugan.dev>
2021-02-16 23:20:17 -0800

    dynamic header for main page

--- client-app/pages/[name].js
+++ client-app/pages/[name].js
@@ -1,20 +1,30 @@
 import React from "react";
+import Head from "next/head";
 import { useGoList } from "../actions/golists";
 
 const GoList = (props) => {
   const { data, isLoading, error } = useGoList(props.name || "demo");
   let content = "";
+  let header = "GoList";
   if (error) {
     content = `Error...${error.message}`;
   } else if (isLoading) {
     content = "Loading...";
   } else {
+    if (data.name) {
+      header = `${header} | ${data.name}`;
+    }
     content = `data: ${JSON.stringify(data)}`;
   }
 
   return (
-    <div className="page-wrapper horizontal-wrapper" id="pageWrapper">
-      {content}
+    <div>
+      <Head>
+        <title>{header}</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+
+      <div>{content}</div>
     </div>
   );
 };
