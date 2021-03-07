df527a4baa809e3b9f4acbbc588ca3bfe9564492
Gan Tu <github@tugan.dev>
2021-03-06 18:49:01 -0800

    run login logic using firebase in root App component

--- admin-app/client/src/components/app.jsx
+++ admin-app/client/src/components/app.jsx
@@ -3,8 +3,13 @@ import Header from "../layout/header";
 import Sidebar from "../layout/sidebar";
 import Footer from "../layout/footer";
 
+import { useDispatch } from "react-redux";
+import { LOG_IN } from "../redux/actionTypes";
+import { firebase_app } from "../data/config";
+
 const App = ({ children }) => {
   console.warn = () => {};
+  const dispatch = useDispatch();
 
   useEffect(() => {
     // reset the body layout, so it won't be affected by box-layout of SHOW page
@@ -12,6 +17,13 @@ const App = ({ children }) => {
       localStorage.getItem("layout_version") || "light"
     }`;
   }, []);
+
+  useEffect(() => {
+    return firebase_app.auth().onAuthStateChanged((user) => {
+      console.log(user);
+      dispatch({ type: LOG_IN, user });
+    });
+  }, [dispatch]);
 
   return (
     <div className="page-wrapper compact-wrapper" id="pageWrapper">
