fbbfb5fd7a280cd09c6ef0bbbf44c00a033e5e78
Gan Tu <github@tugan.dev>
2021-03-06 18:21:18 -0800

    use session store's user info for profile widget logic

--- admin-app/client/src/layout/header/widgets/profile.jsx
+++ admin-app/client/src/layout/header/widgets/profile.jsx
@@ -1,42 +1,32 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment } from "react";
 import man from "../../../assets/images/dashboard/profile.jpg";
 import { LogIn } from "react-feather";
 import { Admin, LogOut } from "../../../constant";
 import { translate } from "react-switch-lang";
 import { useHistory } from "react-router-dom";
-
-import { firebase_app } from "../../../data/config";
+import { useDispatch, useSelector } from "react-redux";
+import { LOG_OUT } from "../../../redux/actionTypes";
 
 const ProfileWidget = (props) => {
   const history = useHistory();
-  const [photoUrl, setPhotoUrl] = useState("");
-  const [name, setName] = useState("Unknown");
+  const dispatch = useDispatch();
+
+  const displayName = useSelector(
+    (store) => store.SessionReducer.user.displayName
+  );
+  const photoUrl = useSelector((store) => store.SessionReducer.user.photoUrl);
 
   const LogOutUser = () => {
-    firebase_app.auth().signOut();
+    dispatch({ type: LOG_OUT });
     history.push(`${process.env.PUBLIC_URL}/login`);
   };
-
-  const saveUserMetadata = (user) => {
-    setName(user?.displayName || user?.email);
-    setPhotoUrl(user?.photoURL);
-  };
-
-  useEffect(() => {
-    const cleanup = firebase_app.auth().onAuthStateChanged(function (user) {
-      if (user) {
-        saveUserMetadata(user);
-      }
-    });
-    return cleanup;
-  }, []);
 
   return (
     <Fragment>
       <div className="media profile-media">
         <img className="b-r-10" src={photoUrl || man} alt="avatar" />
         <div className="media-body">
-          <span>{props.t(name || "Unknown")}</span>
+          <span>{props.t(displayName || "Unknown")}</span>
           <p className="mb-0 font-roboto">
             {props.t(Admin)} <i className="middle fa fa-angle-down"></i>
           </p>
