05cd0af6adf5414a415b88fdfb5de70711d131e1
Gan Tu <github@tugan.dev>
2021-06-17 19:33:38 -0700

    fix: do not use async for jest test

--- backend/tests/home.test.ts
+++ backend/tests/home.test.ts
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../app");
 
 describe("Test the root path: /", () => {
-  test("it should be OK to GET homepage", async () => {
+  test("it should be OK to GET homepage", () => {
     return request(app)
       .get("/")
       .expect(200)
